a3e5933865ec9fbea16e3c43527d4dae795c8a9a
hirendra <hirendrathaku@blackbuck.com>
2016-08-14 23:10:24 +0530

    bug fix

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
 		name = 'QuickDictionary',
 		packages = ['QuickDictionary'],
-		version = '0.4',
+		version = '0.5',
 		description = 'just copy the word for to know its meaning,must be connected to internet',
 		author = 'Hirendra Thakur',
 		author_email = 'hirendrathakur1993@gmail.com',
 		url = 'https://github.com/hirendrathakur/quickDictionary',
-		download_url = 'https://github.com/hirendrathakur/quickDictionary/tarball/0.4',
+		download_url = 'https://github.com/hirendrathakur/quickDictionary/tarball/0.5',
 		keywords = ['dictionary','quick','search','copy'],
 		license = 'MIT',
 		scripts = ['bin/QuickDictionary'],
-		install_requires = ['pyperclip','BeautifulSoup','pynotify','urllib'],
+		install_requires = ['pyperclip','BeautifulSoup','py-notify','urllib'],
 		classifiers = [],
 		)

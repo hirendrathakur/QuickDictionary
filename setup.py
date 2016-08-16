e03f8e4a7b454ac82932963e1d1e1425ffc6490c
hirendra <hirendrathaku@blackbuck.com>
2016-08-16 23:11:37 +0530

    added bs4

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
 		name = 'QuickDictionary',
 		packages = ['QuickDictionary'],
-		version = '0.6',
+		version = '0.7',
 		description = 'just copy the word for to know its meaning,must be connected to internet',
 		author = 'Hirendra Thakur',
 		author_email = 'hirendrathakur1993@gmail.com',
 		url = 'https://github.com/hirendrathakur/quickDictionary',
-		download_url = 'https://github.com/hirendrathakur/quickDictionary/tarball/0.6',
+		download_url = 'https://github.com/hirendrathakur/quickDictionary/tarball/0.7',
 		keywords = ['dictionary','quick','search','copy'],
 		license = 'MIT',
 		scripts = ['bin/QuickDictionary'],
-		install_requires = ['pyperclip','BeautifulSoup','py-notify'],
+		install_requires = ['pyperclip','BeautifulSoup','py-notify','bs4'],
 		classifiers = [],
 		)
